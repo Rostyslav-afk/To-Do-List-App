8ca970318c5e8b44fcdbf4b9aa0f236b5a4377aa
Rostyslav-afk <110056857+Rostyslav-afk@users.noreply.github.com>
2025-06-10 20:50:30 +0200

    js 70% functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-ff
+const inputBox = document.querySelector("#input-box");
+const listContainer = document.querySelector("#list-container");
+const addButton = document.querySelector(".row button");
+
+
+function addTask() {
+    if (inputBox.value === "") {
+        alert("You must write something!");
+    } else {
+        let item = document.createElement("li");
+        item.innerHTML = inputBox.value;
+        listContainer.appendChild(item);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        item.appendChild(span);
+    };
+    inputBox.value = "";
+};
